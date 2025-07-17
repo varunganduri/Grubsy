582b93514a88b69a21f022ecc99b644872dfbf90
varunganduri <varunganduri16@gmail.com>
2025-07-17 17:00:12 +0530

    cors added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import VendorRoutes from "./routes/vendorRoutes.js"
 import FirmRoutes from "./routes/firmRoutes.js"
 import ProductRoutes from "./routes/productRoutes.js"
+import cors from "cors";
 
 const app=express()
 
@@ -13,7 +14,11 @@ mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log("DB connected"))
 .catch((err)=>console.log(err))
 
+
+
 app.use(express.json())
+
+app.use(cors({}))
 
 app.use('/uploads', express.static('uploads'));
 
@@ -21,6 +26,8 @@ app.use('/uploads', express.static('uploads'));
 app.use("/vendors",VendorRoutes)
 app.use("/firms",FirmRoutes)
 app.use("/products",ProductRoutes)
+
+
 
 
 app.listen(2000,()=>{
